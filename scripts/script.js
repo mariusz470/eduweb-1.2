f851b892ddd8377bda6e9429953536f492ba7593
Mariusz Kicinski <mariusz.kicinski@gmail.com>
2017-03-06 15:55:54 +0100

    html done, js initialized

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function ($)({
+	var gridclass = $("div.grid.grid-12");
+	var http = $(".nav a [href^='http']");
+	var radio = $("input:radio:not(:checked), input:checkbox:not(:checked)");
+	var empty = $("div#text p:empty:first");
+	var pagination = $(".pagination-item:not('span')");
+}})
