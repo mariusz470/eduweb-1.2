12ea2f55cbc03adf31c27bd8d34ea28e7bc25e3f
Mariusz Kicinski <mariusz.kicinski@gmail.com>
2017-03-07 00:56:10 +0100

    success, in need of some styling

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,8 @@
     	function cleanbutton(){
     		if (clicked) {
     			$("#result").find( "span:last" ).remove();
+    			$("#info").find( "span:last" ).remove();
+
     		}
     		else {
     			clicked=true
@@ -17,70 +19,31 @@
 		var radio = "$(input:radio:not(:checked), input:checkbox:not(:checked));";
 		var empty = "$(div#text p:empty:first);";
 		var pagination = "$(.pagination-item:not('span'));";
-		var infoResult ="";
 		$("#grid").on("click", function() {
 			cleanbutton();
-			infoResult="grid";
+			$("#info").append('<span>Wszystkie elementy div, posiadające zarówno klasę “grid” oraz klasę “grid-12”.</span>');
     		$("#result").append( $( "<span>"+grid+" </span>" ) );
  			 }
-);
+		);
     	$("#http").on("click", function() {
     		cleanbutton();
-    		infoResult ="http";
+    		$("#info").append('<span>Wszystkie elementy a, których atrybut “href" zaczyna się na “http”, znajdujące się w elemencie o klasie “nav”.</span>');
     		$("#result").append( $( "<span>"+http+" </span>" ) );
   		});
     	$("#radio").on("click", function() {
     		cleanbutton();
-    		infoResult ="radio";
+    		$("#info").append('<span>Wszystkie elementy input, których typ to “radio” lub “checkbox” oraz dodatkowo nie są aktualnie zaznaczone (checked).</span>');
     		$("#result").append( $( "<span>"+radio+" </span>" ) );
   		});
     	$("#empty").on("click", function() {
     		cleanbutton();
-    		infoResult ="empty";
+			$("#info").append('<span>Wyłącznie pierwszy element p, który jest pusty (nie zawiera dzieci) oraz znajduje się w elemencie div z identyfikatorem “text”.</span>');
     		$("#result").append( $( "<span>"+empty+" </span>" ) );
   		});
     	$("#pagination").on("click", function() {
     		cleanbutton();
-    		infoResult ="pagination";
+    		$("#info").append('<span>Wszystkie elementy z klasą “pagination-item”, które nie są elementem span.</span>');
     		$("#result").append( $( "<span>"+pagination+" </span>" ) );
   		});
 	 });
-    function info(event){
-    	switch(event) {
-    		case "grid":
-    		$("#info").find( "span:last" ).remove();
-    		$("#info").append('<span>Wszystkie elementy div, posiadające zarówno klasę “grid” oraz klasę “grid-12”.</span>');
-    		break;
-    		case "http":
-    		$("#info").find( "span:last" ).remove();
-    		$("#info").append('<span>Wszystkie elementy a, których atrybut “href" zaczyna się na “http”, znajdujące się w elemencie o klasie “nav”.</span>');
-    		break;
-    		case "radio":
-    		$("#info").find( "span:last" ).remove();
-    		$("#info").append('<span>Wszystkie elementy input, których typ to “radio” lub “checkbox” oraz dodatkowo nie są aktualnie zaznaczone (checked).</span>');
-    		break;
-    		case "empty":
-    		$("#info").find( "span:last" ).remove();
-    		$("#info").append('<span>Wyłącznie pierwszy element p, który jest pusty (nie zawiera dzieci) oraz znajduje się w elemencie div z identyfikatorem “text”.</span>');
-    		break;
-    		case "pagination":
-    		$("#info").find( "span:last" ).remove();
-    		$("#info").append('<span>Wszystkie elementy z klasą “pagination-item”, które nie są elementem span.</span>');
-    		break;
-    		default:
-    		$("#info").find( "span:last" ).remove();
-    		$("#info").append('<span>Najedź kursorem na przycisk, aby wyświetlić informację. Wciśnij, aby zobaczyć selektor jQuery.</span>');
-    	}
-    }
-    info("");
-    $("#grid").hover(
-    	function(){
-    		$("#info").append(gridInfo)
-    	},info("grid"),info(infoResult));
-    $("#http").hover(info("http"),info(infoResult));
-    $("#radio").hover(info("radio"),info(infoResult));
-    $("#empty").hover(info("empty"),info(infoResult));
-    $("#pagination").hover(info("pagination"),info(infoResult));
-    
-
 })(jQuery);
