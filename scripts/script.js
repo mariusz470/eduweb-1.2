244846f8f78ea27995cc71b2668df9c093d25c8a
Mariusz Kicinski <mariusz.kicinski@gmail.com>
2017-03-06 23:43:48 +0100

    added css

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,45 @@
 (function($) {
 
     $(document).ready(function() {
+    	var clicked=false;
+    	function cleanbutton(){
+    		if (clicked) {
+    			$("#result").find( "span:last" ).remove();
+    		}
+    		else {
+    			clicked=true
+    		}
+    		
+    	}
     	console.log("success");
 		var grid = "$(div.grid.grid-12);";
 		var http = "$(.nav a [href^='http']);";
 		var radio = "$(input:radio:not(:checked), input:checkbox:not(:checked));";
 		var empty = "$(div#text p:empty:first);";
 		var pagination = "$(.pagination-item:not('span'));";
-		$("#grid").hover(function() {
-    $("#result").append( $( "<span>"+grid+" </span>" ) );
-  }, function() {
-    $("#result").find( "span:last" ).remove();
-  }
+		$("#grid").on("click", function() {
+			cleanbutton();
+    		$("#result").append( $( "<span>"+grid+" </span>" ) );
+ 			 }
 );
-    	$("#http").hover(function() {
-    $("#result").append( $( "<span>"+http+" </span>" ) );
-  }, function() {
-    $("#result").find( "span:last" ).remove();
-  }
-);
-    	$("#radio").hover(function() {
-    $("#result").append( $( "<span>"+radio+" </span>" ) );
-  }, function() {
-    $("#result").find( "span:last" ).remove();
-  }
-);
-    	$("#empty").hover(function() {
-    $("#result").append( $( "<span>"+empty+" </span>" ) );
-  }, function() {
-    $("#result").find( "span:last" ).remove();
-  }
-);
-    	$("#pagination").hover(function() {
-    $("#result").append( $( "<span>"+pagination+" </span>" ) );
-  }, function() {
-    $("#result").find( "span:last" ).remove();
-  }
-);
+    	$("#http").on("click", function() {
+    		cleanbutton()
+    		$("#result").append( $( "<span>"+http+" </span>" ) );
+  		});
+    	$("#radio").on("click", function() {
+    		cleanbutton()
+    		$("#result").append( $( "<span>"+radio+" </span>" ) );
+  		});
+    	$("#empty").on("click", function() {
+    		cleanbutton()
+    		$("#result").append( $( "<span>"+empty+" </span>" ) );
+  		});
+    	$("#pagination").on("click", function() {
+    		cleanbutton()
+    		$("#result").append( $( "<span>"+pagination+" </span>" ) );
+  		});
 	 });
+    $("#http").hover()
     
 
 })(jQuery);
